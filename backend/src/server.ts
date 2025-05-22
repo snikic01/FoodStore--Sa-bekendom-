6ec865c066eafa25dedbaa99184d855a37f842f2
Stefan Nikic <stefannikic15@gmail.com>
2025-05-22 17:27:22 +0200

    napravljen server-ts (instalirani npm nodemon -> za brzinu i ts-node -> za konverziju u js

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,2 +1,22 @@
 import express from "express";
 import cors from "cors";
+
+const app = express();
+
+//omogucavanje razgovora medju portovima
+app.use(cors({
+    credentials: true,
+    origin: ["http://localhost:4200"]
+}));
+
+//definisanje get apija
+
+app.get("/api/foods", (req, res) => {
+    res.send("Hello from the server!");
+});
+
+//definisanje porta
+const port = 5000;
+app.listen(port, () => {
+    console.log("Server is running on http://localhost:"  + port);
+});
