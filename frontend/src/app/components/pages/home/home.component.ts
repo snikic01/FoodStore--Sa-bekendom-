b825d178b3092d5c6f99838e2bf6528197527f31
Stefan Nikic <stefannikic15@gmail.com>
2025-05-24 00:28:22 +0200

    Promenjena Home komponenta (foodsObservable)

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -7,6 +7,7 @@ import { RouterLink, RouterModule, ActivatedRoute } from '@angular/router';
 import { SearchComponent } from "../../partials/search/search.component";
 import { TagsComponent } from '../../partials/tags/tags.component';
 import { NotFoundComponent } from '../../partials/not-found/not-found.component';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -18,13 +19,20 @@ export class HomeComponent implements OnInit {
   
   foods:Food[] = [];
   constructor(private foodService:FoodService, activatedRoute:ActivatedRoute) {
+
+    let foodsObservable:Observable<Food[]>;
+
     activatedRoute.params.subscribe((params) => {
       if (params.searchTerm) 
-        this.foods = this.foodService.getAllFoodsBySearchTerm(params.searchTerm);
+        foodsObservable = this.foodService.getAllFoodsBySearchTerm(params.searchTerm);
        else if (params.tag) 
-        this.foods = this.foodService.getAllFoodsByTag(params.tag);
+        foodsObservable = this.foodService.getAllFoodsByTag(params.tag);
        else 
-        this.foods = this.foodService.getAll();
+        foodsObservable = this.foodService.getAll();
+
+        foodsObservable.subscribe((serverFoods) => {
+          this.foods = serverFoods;
+        });
     })
   }
 
